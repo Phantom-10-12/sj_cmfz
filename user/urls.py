13276073651ce76e8a933dfd5893500aa10104ce
善杰 <1293800704@qq.com>
2020-04-09 22:13:24 +0800

    添加注册数量趋势图与全国用户分布图

--- user/urls.py
+++ user/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path("get_all_user/", views.get_all_user, name="get_all_user"),
     path("add_user/", views.add_user, name="add_user"),
     path("edit_user/", views.edit_user, name="edit_user"),
+    path("user_distribution/", views.user_distribution, name="user_distribution"),
+    path("user_register_trend/", views.user_register_trend, name="user_register_trend"),
 ]
